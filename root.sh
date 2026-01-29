cf501606af7a8f12dd96ffc8ba5ca3ced5b81de2
Ivaylo Dionisov <ivaylo.dionisov@cern.ch>
2026-01-29 20:58:10 +0200

    feat: Update root.sh

--- root.sh
+++ root.sh
@@ -1,9 +1,8 @@
 package: ROOT
-version: "%(tag_basename)s%(defaults_upper)s"
-tag: v5-34-30-alice7
+version: "%(tag_basename)s"
+tag: v6-36-04-alice1
 source: https://github.com/alisw/root
 requires:
-  - AliEn-Runtime:(?!.*ppc64)
   - GSL
   - opengl:(?!osx)
   - Xdevel:(?!osx)
@@ -11,6 +10,13 @@ requires:
   - "MySQL:slc7.*"
   - GCC-Toolchain:(?!osx)
   - libxml2
+  - zlib
+  - libpng
+  - protobuf
+  - FFTW3
+  - XRootD
+  - pythia
+  - pythia6
 build_requires:
   - CMake
   - "Xcode:(osx.*)"
@@ -20,14 +26,6 @@ env:
 prepend_path:
   PYTHONPATH: "$ROOTSYS/lib"
 incremental_recipe: |
-  if [[ $ALICE_DAQ ]]; then
-    export ROOTSYS=$BUILDDIR && make ${JOBS+-j$JOBS} && make static
-    for S in montecarlo/vmc tree/treeplayer io/xmlparser math/minuit2 sql/mysql; do
-      mkdir -p $INSTALLROOT/$S/src
-      cp -v $S/src/*.o $INSTALLROOT/$S/src/
-    done
-    export ROOTSYS=$INSTALLROOT
-  fi
   make ${JOBS:+-j$JOBS} install
   mkdir -p $INSTALLROOT/etc/modulefiles && rsync -a --delete etc/modulefiles/ $INSTALLROOT/etc/modulefiles
   cd $INSTALLROOT/test
@@ -36,27 +34,16 @@ incremental_recipe: |
 #!/bin/bash -e
 unset ROOTSYS
 
-
-ALIBUILD_GCC_VERSION=11
-
-export CC=gcc-11
-export CXX=g++-11
-export CXXFLAGS="-I/usr/include/c++/11 -I/usr/include -I$SOURCEDIR/core/clib/inc $CXXFLAGS"
-export CXXFLAGS="$CXXFLAGS -DHAVE_STRLCPY -DHAVE_STRLCAT"
-
-export CMAKE_CXX_COMPILER=g++-11
-export CMAKE_C_COMPILER=gcc-11
-export CMAKE_LINKER=g++-11
-
-# ROOT's core/clib/inc/strlcpy.h contains declaration that don't match
-# newer systemc's noexcept signatures, causing compilation errors
-if grep -q "strlcpy" /usr/include/string.h; then
-    if [ -f "$SOURCEDIR/core/clib/inc/strlcpy.h" ]; then
-        sed -i 's/size_t strlcpy/size_t strlcpy_renamed/' "$SOURCEDIR/core/clib/inc/strlcpy.h"
-        sed -i 's/size_t strlcat/size_t strlcat_renamed/' "$SOURCEDIR/core/clib/inc/strlcpy.h"
-    fi
+if [ -n "$VIRTUAL_ENV" ]; then
+  echo "Building with Virtual Environment: $VIRTUAL_ENV"
+  export PATH="$VIRTUAL_ENV/bin:$PATH"
+  export PYTHON_EXECUTABLE="$VIRTUAL_ENV/bin/python3"
+else
+  export PYTHON_EXECUTABLE=$(which python3)
 fi
-
+COMPILER_CC=cc
+COMPILER_CXX=c++
+COMPILER_LD=c++
 case $PKGVERSION in
   v6-20*) 
      [[ "$CXXFLAGS" == *'-std=c++11'* ]] && CMAKE_CXX_STANDARD=11 || true
@@ -67,9 +54,16 @@ case $PKGVERSION in
     [[ "$CXXFLAGS" == *'-std=c++11'* ]] && CXX11=1 || true
     [[ "$CXXFLAGS" == *'-std=c++14'* ]] && CXX14=1 || true
     [[ "$CXXFLAGS" == *'-std=c++17'* ]] && CXX17=1 || true
+    [[ "$CXXFLAGS" == *'-std=c++20'* ]] && CMAKE_CXX_STANDARD=20 || true
+    [[ "$CXXFLAGS" == *'-std=c++23'* ]] && CMAKE_CXX_STANDARD=23 || true
   ;;
 esac
 
+# We do not use global options for ROOT, otherwise the -g will
+# kill compilation on < 8GB machines
+unset CXXFLAGS
+unset CFLAGS
+unset LDFLAGS
 
 case $ARCHITECTURE in
   osx*)
@@ -88,79 +82,59 @@ then
     PYHIA6_LATE=TRUE
 fi
 
-if [[ $ALICE_DAQ ]]; then
-  # DAQ requires static ROOT, only supported by ./configure (not CMake).
-  export ROOTSYS=$BUILDDIR
-  $SOURCEDIR/configure                  \
-    --with-pythia6-uscore=SINGLE        \
-    --enable-minuit2                    \
-    --enable-roofit                     \
-    --enable-soversion                  \
-    --enable-builtin-freetype           \
-    --enable-builtin-pcre               \
-    --enable-mathmore                   \
-    --with-f77=gfortran                 \
-    --with-cc=$COMPILER_CC              \
-    --with-cxx=$COMPILER_CXX            \
-    --with-ld=$COMPILER_LD              \
-    ${CXXFLAGS:+--cxxflags="$CXXFLAGS"} \
-    --disable-shadowpw                  \
-    --disable-astiff                    \
-    --disable-globus                    \
-    --disable-krb5                      \
-    --disable-ssl                       \
-    --enable-mysql
-  FEATURES="builtin_freetype builtin_pcre mathmore minuit2 pythia6 roofit
-            soversion ${CXX11:+cxx11} ${CXX14:+cxx14} mysql xml"
-else
-  # Normal ROOT build.
-  cmake $SOURCEDIR                                                \
+case $PKG_VERSION in
+  v6[-.]2[0-9]*) EXTRA_CMAKE_OPTIONS="-Dminuit2=ON -Dpythia6=ON -Dpythia6_nolink=ON" ;;
+  v6[-.]36[-.][0-9]*) EXTRA_CMAKE_OPTIONS="-Dminuit=ON -Dpythia6=ON -Dpythia6_nolink=ON -Dproof=ON -Dgeombuilder=ON" ;;
+  *) EXTRA_CMAKE_OPTIONS="-Dminuit=ON" ;;
+esac
+
+# Normal ROOT build.
+cmake $SOURCEDIR                                                  \
         -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE                      \
         -DCMAKE_INSTALL_PREFIX=$INSTALLROOT                       \
-        ${ALIEN_RUNTIME_ROOT:+-Dalien=ON}                         \
-        ${ALIEN_RUNTIME_ROOT:+-DALIEN_DIR=$ALIEN_RUNTIME_ROOT}    \
-        ${ALIEN_RUNTIME_ROOT:+-DMONALISA_DIR=$ALIEN_RUNTIME_ROOT} \
         ${XROOTD_ROOT:+-DXROOTD_ROOT_DIR=$XROOTD_ROOT}            \
-	${ALIEN_RUNTIME_ROOT:+-DXROOTD_ROOT_DIR=$ALIEN_RUNTIME_ROOT}\
-	-DCMAKE_CXX_STANDARD=$CMAKE_CXX_STANDARD                  \
+        -DCMAKE_CXX_STANDARD=$CMAKE_CXX_STANDARD                  \
         -Dfreetype=ON                                             \
         -Dbuiltin_freetype=OFF                                    \
         -Dpcre=OFF                                                \
         -Dbuiltin_pcre=ON                                         \
+        -Dsqlite=OFF                                              \
         -Drpath=ON                                                \
         ${ENABLE_COCOA:+-Dcocoa=ON}                               \
+        ${EXTRA_CMAKE_OPTIONS}                                    \
         -DCMAKE_CXX_COMPILER=$COMPILER_CXX                        \
         -DCMAKE_C_COMPILER=$COMPILER_CC                           \
         -DCMAKE_LINKER=$COMPILER_LD                               \
         ${GCC_TOOLCHAIN_VERSION:+-DCMAKE_EXE_LINKER_FLAGS="-L$GCC_TOOLCHAIN_ROOT/lib64"} \
-        ${OPENSSL_ROOT:+-DOPENSSL_ROOT=$ALIEN_RUNTIME_ROOT}       \
         ${SYS_OPENSSL_ROOT:+-DOPENSSL_ROOT=$SYS_OPENSSL_ROOT}     \
-        ${SYS_OPENSSL_ROOT:+-DOPENSSL_INCLUDE_DIR=$SYS_OPENSSL_ROOT/include}  \
-        ${LIBXML2_ROOT:+-DLIBXML2_ROOT=$ALIEN_RUNTIME_ROOT}       \
+        ${SYS_OPENSSL_ROOT:+-DOPENSSL_INCLUDE_DIR=$SYS_OPENSSL_ROOT/include} \
         ${GSL_ROOT:+-DGSL_DIR=$GSL_ROOT}                          \
-	${PYTHIA_ROOT:+-DPYTHIA8_DIR=$PYTHIA_ROOT}                \
-	${PYTHIA6_ROOT:+-DPYTHIA6_LIBRARY=$PYTHIA6_ROOT/lib/libpythia6.so}\
-	${PYTHIA6_ROOT:+-Dpythia6=ON}                             \
+        ${LIBPNG_ROOT:+-DPNG_INCLUDE_DIRS="${LIBPNG_ROOT}/include"} \
+        ${LIBPNG_ROOT:+-DPNG_LIBRARY="${LIBPNG_ROOT}/lib/libpng.${SONAME}"} \
+        ${PROTOBUF_REVISION:+-DProtobuf_DIR=${PROTOBUF_ROOT}}     \
+        ${ZLIB_ROOT:+-DZLIB_ROOT=${ZLIB_ROOT}}                    \
+        ${FFTW3_ROOT:+-DFFTW_DIR=${FFTW3_ROOT}}                   \
+        -Dfftw3=ON                                                \
+        -Dbuiltin_fftw3=OFF                                       \
+        ${PYTHIA_ROOT:+-DPYTHIA8_DIR=$PYTHIA_ROOT}                \
+        ${PYTHIA_ROOT:+-Dpythia8=ON}                              \
+        ${PYTHIA6_LATE:+-Dpythia6_nolink=ON}                      \
         -Dpgsql=OFF                                               \
-        -Dminuit2=ON                                              \
         -Dgdml=ON                                                 \
-	${PYTHIA6_LATE:+-Dpythia6_nolink=ON}                      \
+        -Dmathmore=ON                                             \
         -Droofit=ON                                               \
         -Dhttp=ON                                                 \
         -Dsoversion=ON                                            \
         -Dshadowpw=OFF                                            \
         -Dvdt=ON                                                  \
         -Dbuiltin_vdt=ON                                          \
-        ${PYTHON_ROOT:+-DPYTHON_EXECUTABLE=$PYTHONHOME/bin/python} \
-        ${PYTHON_ROOT:+-DPYTHON_INCLUDE_DIR=$PYTHONHOME/include/python3.6m} \
-        ${PYTHON_ROOT:+-DPYTHON_LIBRARY=$PYTHONHOME/lib/libpython3.6m.so} \
-        -DCMAKE_PREFIX_PATH="$FREETYPE_ROOT;$SYS_OPENSSL_ROOT;$GSL_ROOT;$ALIEN_RUNTIME_ROOT;$PYTHON_ROOT;$PYTHON_MODULES_ROOT"
-  FEATURES="builtin_pcre mathmore xml ssl opengl minuit2 http gdml ${PYTHIA_ROOT:+pythia8}
+        -DPYTHON_EXECUTABLE=$PYTHON_EXECUTABLE                    \
+        -DCMAKE_PREFIX_PATH="$FREETYPE_ROOT;$SYS_OPENSSL_ROOT;$GSL_ROOT;$PYTHON_ROOT;$PYTHON_MODULES_ROOT,\
+                             $FFTW_ROOT"
+FEATURES="builtin_pcre mathmore xml ssl opengl http gdml fftw3 ${PYTHIA_ROOT:+pythia8}
             pythia6 roofit soversion vdt ${CXX11:+cxx11} ${CXX14:+cxx14} ${XROOTD_ROOT:+xrootd}
-            ${ALIEN_RUNTIME_ROOT:+alien monalisa}
             ${ENABLE_COCOA:+builtin_freetype}"
-  NO_FEATURES="${FREETYPE_ROOT:+builtin_freetype}"
-fi
+NO_FEATURES="${FREETYPE_ROOT:+builtin_freetype}"
 
 # Check if all required features are enabled
 bin/root-config --features
@@ -171,17 +145,6 @@ for FEATURE in $NO_FEATURES; do
   bin/root-config --has-$FEATURE | grep -q no
 done
 
-if [[ $ALICE_DAQ ]]; then
-  make ${JOBS+-j$JOBS}
-  make static
-  # *.o files from these modules need to be copied to the install directory
-  # because AliRoot static build uses them directly
-  for S in montecarlo/vmc tree/treeplayer io/xmlparser math/minuit2 sql/mysql; do
-    mkdir -p $INSTALLROOT/$S/src
-    cp -v $S/src/*.o $INSTALLROOT/$S/src/
-  done
-  export ROOTSYS=$INSTALLROOT
-fi
 make ${JOBS+-j$JOBS} install
 [[ -d $INSTALLROOT/test ]] && ( cd $INSTALLROOT/test && env PATH=$INSTALLROOT/bin:$PATH LD_LIBRARY_PATH=$INSTALLROOT/lib:$LD_LIBRARY_PATH DYLD_LIBRARY_PATH=$INSTALLROOT/lib:$DYLD_LIBRARY_PATH make ${JOBS+-j$JOBS} )
 
@@ -196,8 +159,7 @@ proc ModulesHelp { } {
 set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
 module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
 # Dependencies
-module load BASE/1.0 ${ALIEN_RUNTIME_ROOT:+AliEn-Runtime/$ALIEN_RUNTIME_VERSION-$ALIEN_RUNTIME_REVISION}        \\
-                     ${GCC_TOOLCHAIN_VERSION:+GCC-Toolchain/$GCC_TOOLCHAIN_VERSION-$GCC_TOOLCHAIN_REVISION}     \\
+module load BASE/1.0 ${GCC_TOOLCHAIN_VERSION:+GCC-Toolchain/$GCC_TOOLCHAIN_VERSION-$GCC_TOOLCHAIN_REVISION}     \\
                      ${GSL_VERSION:+GSL/$GSL_VERSION-$GSL_REVISION}                                             \\
                      ${XROOTD_VERSION:+XRootD/$XROOTD_VERSION-$XROOTD_REVISION}                                 \\
                      ${FREETYPE_VERSION:+FreeType/$FREETYPE_VERSION-$FREETYPE_REVISION}                         \\
