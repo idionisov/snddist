c84169832a5dca7226bbe365750c8fe047248623
Ivaylo Dionisov <ivaylo.dionisov@cern.ch>
2026-01-29 20:56:58 +0200

    feat: Update cgal.sh

--- cgal.sh
+++ cgal.sh
@@ -1,5 +1,5 @@
 package: cgal
-version: "v4.6.3%(defaults_upper)s"
+version: "4.6.3"
 requires:
   - boost
 build_requires:
@@ -15,17 +15,16 @@ case $ARCHITECTURE in
     [[ ! $BOOST_ROOT ]] && BOOST_ROOT=`brew --prefix boost`
   ;;
 esac
-PKGID=35136
-URL="https://gforge.inria.fr/frs/download.php/file/${PKGID}/"
+URL="https://github.com/CGAL/cgal/releases/download/releases%2FCGAL-$PKGVERSION/CGAL-$PKGVERSION.tar.xz"
 
-curl -Lo cgal.tar.bz2 "$URL"
-tar xjf cgal.tar.bz2
+curl -kLo cgal.tar.xz "$URL"
+tar xJf cgal.tar.xz
 cd CGAL-*
 
 if [[ "$BOOST_ROOT" != '' ]]; then
   export LDFLAGS="-L$BOOST_ROOT/lib"
   export LD_LIBRARY_PATH="$BOOST_ROOT/lib:$LD_LIBRARY_PATH"
-  export DYLD_LIBRARY_PATH="$BOOST_ROOT/lib:$DYLD_LIBRARY_PATH"
+  export DYLD_LIBRARY_PATH="$BOOST_ROOT/lib:$LD_LIBRARY_PATH"
 fi
 
 export MPFR_LIB_DIR="${MPFR_ROOT}/lib"
@@ -35,12 +34,14 @@ export GMP_INC_DIR="${GMP_ROOT}/include"
 
 cmake . \
       -DCMAKE_INSTALL_PREFIX:PATH="${INSTALLROOT}" \
-      -DCMAKE_SKIP_RPATH:BOOL=YES \
+      -DCMAKE_INSTALL_LIBDIR:PATH="lib" \
+      -DCMAKE_BUILD_TYPE=Release \
       -DWITH_BLAS:BOOL=OFF \
       -DWITH_CGAL_Core:BOOL=ON \
       -DWITH_CGAL_ImageIO:BOOL=ON \
       -DWITH_CGAL_Qt3:BOOL=OFF \
       -DWITH_CGAL_Qt4:BOOL=OFF \
+      -DWITH_CGAL_Qt5:BOOL=OFF \
       -DWITH_Coin3D:BOOL=OFF \
       -DWITH_ESBTL:BOOL=OFF \
       -DWITH_Eigen3:BOOL=OFF \
@@ -69,6 +70,10 @@ cmake . \
 make VERBOSE=1 ${JOBS:+-j$JOBS}
 make install VERBOSE=1
 
+find $INSTALLROOT/lib/ -name "*.dylib" -exec install_name_tool -add_rpath @loader_path/../lib {} \;
+find $INSTALLROOT/lib/ -name "*.dylib" -exec install_name_tool -add_rpath ${INSTALLROOT}/lib {} \;
+find $INSTALLROOT/lib/ -name "*.dylib" -exec install_name_tool -id {} {} \;
+
 # Modulefile
 MODULEDIR="$INSTALLROOT/etc/modulefiles"
 MODULEFILE="$MODULEDIR/$PKGNAME"
@@ -82,7 +87,7 @@ proc ModulesHelp { } {
 set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
 module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
 # Dependencies
-module load BASE/1.0 ${BOOST_VERSION:+boost/$BOOST_VERSION-$BOOST_REVISION}
+module load BASE/1.0 ${BOOST_REVISION:+boost/$BOOST_VERSION-$BOOST_REVISION}
 # Our environment
 setenv CGAL_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
 prepend-path PATH \$::env(CGAL_ROOT)/bin
