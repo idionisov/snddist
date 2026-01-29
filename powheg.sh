05cc36232ed3078759d061f0acaf2aab9e55b84e
Ivaylo Dionisov <ivaylo.dionisov@cern.ch>
2026-01-29 20:57:59 +0200

    feat: Update powheg.sh

--- powheg.sh
+++ powheg.sh
@@ -1,11 +1,12 @@
 package: POWHEG
-version: "%(tag_basename)s%(defaults_upper)s"
-source: https://github.com/alisw/POWHEG
-tag: "r3178-alice1"
+version: "%(tag_basename)s"
+source: https://github.com/SND-LHC/POWHEG
+tag: "r4110-forward-snd"
 requires:
   - fastjet
   - "GCC-Toolchain:(?!osx|slc5)"
-  - lhapdf5
+  - lhapdf
+  - looptools
 ---
 #!/bin/bash -e
 
@@ -15,7 +16,7 @@ install -d ${INSTALLROOT}/bin
 
 export LIBRARY_PATH="$LD_LIBRARY_PATH"
 
-PROCESSES="${FASTJET_VERSION:+dijet }hvq W Z"
+PROCESSES="${FASTJET_VERSION:+trijet }${FASTJET_VERSION:+dijet }hvq W Z directphoton"
 for proc in ${PROCESSES}; do
     mkdir ${proc}/{obj,obj-gfortran}
     make -C ${proc}
@@ -23,23 +24,15 @@ for proc in ${PROCESSES}; do
 done
 
 # Modulefile
-MODULEDIR="$INSTALLROOT/etc/modulefiles"
-MODULEFILE="$MODULEDIR/$PKGNAME"
-mkdir -p "$MODULEDIR"
-cat > "$MODULEFILE" <<EoF
-#%Module1.0
-proc ModulesHelp { } {
-  global version
-  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
-}
-set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
-module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
-# Dependencies
-module load BASE/1.0 lhapdf5/$LHAPDF5_VERSION-$LHAPDF5_REVISION ${GCC_TOOLCHAIN_ROOT:+GCC-Toolchain/$GCC_TOOLCHAIN_VERSION-$GCC_TOOLCHAIN_REVISION} ${FASTJET_VERSION:+fastjet/$FASTJET_VERSION-$FASTJET_REVISION}
+mkdir -p etc/modulefiles
+alibuild-generate-module > etc/modulefiles/$PKGNAME
+cat >> etc/modulefiles/$PKGNAME <<EoF
+
 # Our environment
 setenv POWHEG_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
 setenv Powheg_INSTALL_PATH \$::env(POWHEG_ROOT)/lib/Powheg
+setenv POWHEG_HVQ  \$::env(POWHEG_ROOT)/POWHEG/hvq
 prepend-path PATH \$::env(POWHEG_ROOT)/bin
 prepend-path LD_LIBRARY_PATH \$::env(POWHEG_ROOT)/lib/Powheg
-$([[ ${ARCHITECTURE:0:3} == osx ]] && echo "prepend-path DYLD_LIBRARY_PATH \$::env(POWHEG_ROOT)/lib/Powheg")
 EoF
+mkdir -p $INSTALLROOT/etc/modulefiles && rsync -a --delete etc/modulefiles/ $INSTALLROOT/etc/modulefiles
